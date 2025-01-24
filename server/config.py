c5dca5b766512b5b8b34cca76d19501c581a3624
+ <jacqueline.trapp18@gmail.com>
2025-01-24 16:05:26 -0500

    adding requirements.txt

--- server/config.py
+++ server/config.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Setup to serve React build
 app = Flask(
