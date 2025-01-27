91d684e7bd5087705e5454a8e25a486dacf20e96
+ <jacqueline.trapp18@gmail.com>
2025-01-26 21:05:44 -0500

    updating app.py to redirect to index.html

--- server/app.py
+++ server/app.py
@@ -4,12 +4,16 @@ from models import CartItem, Cookie, Favorite, Order, User, Review
 from config import app, db, api
 from datetime import datetime
 # from flask_migrate import Migrate
-from flask import request, jsonify, session, make_response
+from flask import request, jsonify, session, make_response, render_template
 from flask_restful import  Resource
 # import os
 
 # BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 @app.before_request
 def check_if_logged_in():
